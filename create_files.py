bd1c8b8d0c5ca833d19939d95759bfc1ef633f44
flubknuy <spencergwwilliams@gmail.com>
2018-08-01 11:09:58 -0500

    debugging

--- create_files.py
+++ create_files.py
@@ -6,13 +6,13 @@ global jack
 jack = "All work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play makes Jack a dull boy/nAll work and no play ma"
 
 def main():
+    print("RUNNING PYTHON SCRIPT")
     make_file()
 
 def make_file():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     name = "file_%05d.txt" % rank
-    print()
     os.system('echo ' + jack + ' > ' + name)
 
 
