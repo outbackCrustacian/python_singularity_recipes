f7a56a3c51ae9585b078409827052f206add4190
flubknuy <spencergwwilliams@gmail.com>
2018-08-01 09:00:22 -0500

    debugging

--- create_files.py
+++ create_files.py
@@ -31,13 +31,10 @@ def main():
     make_file()
 
 def make_file():
-    for r in range(100):
-        comm = MPI.COMM_WORLD
-        rank = comm.Get_rank()
-        if(r == rank):
-            name = "file_%05d.txt" % rank
-            print(name)
-            os.system(jack + ' > ' + name)
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    name = "file_%05d.txt" % rank
+    os.system(jack + ' > ' + name)
 
 
 if __name__ == "__main__":
